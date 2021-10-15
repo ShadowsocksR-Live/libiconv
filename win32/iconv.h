93cc9d2bac80665d4651006d59aa0760057e3806
ssrlive <30760636+ssrlive@users.noreply.github.com>
2021-10-15 22:26:35 +0800

    Update iconv.h

--- win32/iconv.h
+++ win32/iconv.h
@@ -24,7 +24,7 @@
 
 #if 1 && BUILDING_LIBICONV
 #define LIBICONV_DLL_EXPORTED __attribute__((__visibility__("default")))
-#elif defined _MSC_VER && BUILDING_LIBICONV
+#elif defined(_MSC_VER) && BUILDING_LIBICONV
 #define LIBICONV_DLL_EXPORTED __declspec(dllexport)
 #else
 #define LIBICONV_DLL_EXPORTED
